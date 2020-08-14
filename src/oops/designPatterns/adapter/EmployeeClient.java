563befb275bb205106c3a1aecefbe377d5d23017
Rajat Goyal <rajatgoyal715@gmail.com>
2020-08-15 02:10:29 +0530

    Add adapter pattern

--- src/oops/designPatterns/adapter/EmployeeClient.java
+++ src/oops/designPatterns/adapter/EmployeeClient.java
@@ -25,7 +25,9 @@ public class EmployeeClient {
 		
 		//We have to add the data of all the employees to the employees list
 		//But it only accepts objects of type Employee
-		
+		employees.add(employeeFromDB);
+		employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+		employees.add(new EmployeeAdapterCSV(employeeFromCSV));
 		
 		return employees;
 		
